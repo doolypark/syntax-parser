5bfff5ee3f358387979426d09ee3adc1d81b2641
doolypark <72215352+doolypark@users.noreply.github.com>
2021-09-26 21:51:45 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// John Park (R#11487176) | Assingment #3 | 10/14/2020
+// John Park (R#11487176) | Assingment #4 | 11/01/2020
 //
 // This program accepts a file through argv and iterates through functions to         print out lexemes and its corresponding tokens. 
 //
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 // classification of char
 #define LETTER 0
@@ -53,22 +54,41 @@ void getChar();
 void getNonBlank();
 int lex();
 
+//parser prototypes
+void stmt();
+void compare();
+void expr();
+void term();
+void factor();
+
 
 // Attempts to open file, if it opens succesfully, calls lex() while nextToken is     not EOF, otherwise print an error message
 int main(int argc, char *argv[]) 
 {
     /* Open the input data file and process its contents */
-    if ((in_fp = fopen(argv[1], "r")) == NULL) {
-        printf("ERROR - cannot open file \n");
-    } else {
-        getChar(); 
-        printf("DanC Analyzer :: R11487176\n\n\n");
-        do{
-          lex();        
-        }while(nextToken != EOF);
-          
+    // gives exit code 2 if no file input
+    if(argc == 1){
+      exit(2);
     }
-
+    // if there are 2 argc, check if the file is not NULL
+    else if(argc == 2){
+      if ((in_fp = fopen(argv[1], "r")) != NULL) {
+        getChar(); 
+        printf("DanC Parser :: R11487176\n\n\n");
+        do{
+          lex();
+          stmt();
+          printf("Syntax validated\n");  
+          printf("exit 0\n");
+          exit(0);     
+        }while(nextToken != EOF);      
+    }
+    // if file is NULL, exit code 3
+    else if ((in_fp = fopen(argv[1], "r")) == NULL){
+      printf("exit3");
+      exit(3);
+    }   
+  }
     return 0;
 }
 
@@ -310,14 +330,215 @@ int lex() {
     else if(nextToken == UNKNOWN){
       strcpy(tokeName, "UNKNOWN");
     }
-
+    else if(nextToken == -1){
+      strcpy(tokeName, "EOF");
+    }
+    
+    /*
     // prints lexeme and tokennames if token isn't EOF
     if(nextToken != EOF){
       printf("%-10s\t\t%s\n", lexeme, tokeName);
     }
+    */
     
     
     
     return nextToken;
 } /* End of function lex */
 
+/* start of parser */
+
+void stmt()
+{
+
+  // root of the parsing tree
+  
+  // check for identifier
+  if(nextToken == IDENT){
+    lex();
+    // checks for := after IDENT
+    if (nextToken == ASSIGN_OP){
+      lex();
+      expr();
+      // checks condition if there is a semicolon 
+      if(nextToken == SEMICOLON){
+        lex();
+        if(nextToken >= 0){
+          stmt();
+          }
+          else{
+            printf("ERROR encounter: The next lexeme was %s\n", tokeName);
+            exit(1);
+          }
+      }
+      // checks condition if there is no semicolon 
+      else{
+        if(nextToken >= 0){
+          printf("ERROR encounter: The next lexeme was %s\n", tokeName);
+          exit(1);
+        }
+        else{
+          stmt();
+        }
+      }
+    }
+    else{
+        printf("ERROR encounter: The next lexeme was %s\n", tokeName);
+        exit(1);
+    }
+  }
+  // check for KEY and WRITE key
+  else if (nextToken == KEY_READ || nextToken == KEY_WRITE){
+    lex();
+    expr();
+    // checks condition if there is a semicolon
+    if(nextToken == SEMICOLON){
+        lex();
+        if(nextToken >= 0){
+          stmt();
+          }
+          else{
+            printf("ERROR encounter: The next lexeme was %s\n", tokeName);
+            exit(1);
+          }
+      }
+      // checks condition if there is no semicolon
+      else{
+        if (nextToken >= 0){
+          printf("error encounter: The lexeme was %s\n", tokeName);
+          exit(1);
+        }
+        else{
+          stmt();
+        }
+      }
+  }
+  // check for WHILE key
+  else if (nextToken == KEY_WHILE){
+    lex();
+    compare();
+    // check if there is DO key in while
+    if(nextToken == KEY_DO){
+      lex();
+      stmt();
+      // check if od key is at the end of while
+      if(nextToken == KEY_OD){
+        lex();
+        // check if there is a semicolon after od
+        if(nextToken == SEMICOLON){
+        lex();
+        if(nextToken >= 0){
+          stmt();
+          }
+          else{
+            printf("ERROR encounter: The next lexeme was %s\n", tokeName);
+            exit(1);
+          }
+      }
+      // conditions if there is no semicolon after od
+      else{
+        if (nextToken >= 0){
+          printf("error encounter: The lexeme was %s\n", tokeName);
+          exit(1);
+        }
+        else{
+          stmt();
+        }
+      }
+      }
+    }  
+  }
+}
+
+void compare()
+{
+  // comparison = expr > expr | expr < expr | expr = expr | expr <> expr |
+  // expr <= expr | expr >= expr
+  
+  // parses first expr
+  expr();
+  
+  // checks after the first parsed expr
+  if(nextToken == GREATER_OP){
+    lex();
+    expr();
+  }
+  else if(nextToken == LESSER_OP){
+    lex();
+    expr();
+  }
+  else if(nextToken == EQUAL_OP){
+    lex();
+    expr();
+  }
+  else if(nextToken == NEQUAL_OP){
+    lex();
+    expr();
+  }
+  else if(nextToken == LEQUAL_OP){
+    lex();
+    expr();
+  }
+  else if(nextToken == GEQUAL_OP){
+    lex();
+    expr();
+  }
+  
+}
+  
+void expr()
+{
+  // expr = <term> {+ | - <term>}
+  
+  // parses the first term
+  term();
+
+  // in a while loop because once it appears once, it can appear as many times
+  while (nextToken == ADD_OP || nextToken == SUB_OP){
+    lex();
+    term();
+  } 
+}
+
+void term()
+{
+  // term = <factor> {* | / <factor>} 
+  
+  // parses first factor
+  factor();
+
+  // parses rest of term if next lex is * | /
+  while(nextToken == MULT_OP || nextToken == DIV_OP){
+    lex();
+    factor();
+  } 
+}
+
+
+void factor()
+{
+  //factor = (<expr>) | INT_LIT | IDENT |
+
+  if (nextToken == INT_LIT || nextToken == IDENT){
+    lex();
+  }
+  //checks for left parenthesis
+  else if (nextToken == LEFT_PAREN){
+    lex();
+    expr();
+    // checks right right parenthesis
+    if (nextToken == RIGHT_PAREN){
+      lex();
+    }
+    // prints error if expected right paren was not there
+    else{
+      printf("Error encounter: The next lexeme was %d\n", nextToken);
+      exit(1);
+    }
+  }
+  else{
+    printf("ERROR encounter: The next lexeme was %s\n", tokeName);
+    exit(1); // when RHS isnt expr, IDENT, or INT_LIT
+  }
+
+}
